d3364be73897190907615b87e49168a205cde75d
sawyerbutton <lil103@pitt.edu>
2018-10-29 10:15:57 +0800

    added display component with remove function for blockchain coin by pure function filter

--- src/app/blockchain.reducer.ts
+++ src/app/blockchain.reducer.ts
@@ -2,11 +2,13 @@ import { BlockchainModel } from './blockchain/blockchain.model';
 import { Action } from '@ngrx/store';
 
 export const ADD_COIN = 'ADD_COIN';
-
+export const REMOVE_COIN = 'REMOVE_COIN';
 export function addCoinReducer(state: BlockchainModel[] = [], action) {
   switch (action.type) {
     case ADD_COIN:
       return [...state, action.payload];
+    case REMOVE_COIN:
+      return state.filter(item => item !== action.payload);
     default:
       return state;
   }
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {BlockchainModel} from '../blockchain/blockchain.model';
+import {select, Store} from '@ngrx/store';
+import {AppState} from '../app.state';
+import {REMOVE_COIN} from '../blockchain.reducer';
 
 @Component({
   selector: 'app-display',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-
-  constructor() { }
+  coins$: Observable<BlockchainModel[]>;
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+    this.coins$ = this.store.pipe(select(state => state.blockchain));
+  }
+
+  removeCoin(payload) {
+    if (confirm(`Are you sure to delete the coin ${payload.name}`)) {
+      this.store.dispatch({
+        type: REMOVE_COIN,
+        payload: payload
+      });
+    }
   }
 
 }
